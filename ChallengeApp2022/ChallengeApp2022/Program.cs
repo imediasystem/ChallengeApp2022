cf716d4209849fecf6ae0baf75caa4a19a1d821a
iMedia SysteM <125789515+imediasystem@users.noreply.github.com>
2023-12-17 20:18:02 +0100

    Day 4

--- ChallengeApp2022/ChallengeApp2022/Program.cs
+++ ChallengeApp2022/ChallengeApp2022/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello Kris!");
+﻿// Zadanie domowe Day 4
+
+string name = "Ewa";
+string sex = "kobieta";
+var age = 30;
+
+if(name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if(sex == "kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if(sex != "kobieta" && age < 18)
+{
+    Console.WriteLine("Niepełnoleni mężczyzna");
+}
