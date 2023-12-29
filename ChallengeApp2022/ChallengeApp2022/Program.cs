cc05b63b60ba19c63400a5ba09b0e7271d552c7f
iMedia SysteM <125789515+imediasystem@users.noreply.github.com>
2023-12-29 21:06:18 +0100

    Poprawa zadania domowego Day 5

--- ChallengeApp2022/ChallengeApp2022/Program.cs
+++ ChallengeApp2022/ChallengeApp2022/Program.cs
@@ -2,71 +2,71 @@
 
 int number = 3633;
 string numberAsString = number.ToString();
-char[] signs = numberAsString.ToArray();
+char[] digits = numberAsString.ToArray();
 
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
+int count0 = 0;
+int count1 = 0;
+int count2 = 0;
+int count3 = 0;
+int count4 = 0;
+int count5 = 0;
+int count6 = 0;
+int count7 = 0;
+int count8 = 0;
+int count9 = 0;
 
-foreach (char sign in signs)
+foreach (char digit in digits)
 {
-    if (sign == '0')
+    if (digit == '0')
     {
-        counter0++;
+        count0++;
     }
-    else if (sign == '1')
+    else if (digit == '1')
     {
-        counter1++;
+        count1++;
     }
-    else if (sign == '2')
+    else if (digit == '2')
     {
-        counter2++;
+        count2++;
     }
-    else if (sign == '3')
+    else if (digit == '3')
     {
-        counter3++;
+        count3++;
     }
-    else if (sign == '4')
+    else if (digit == '4')
     {
-        counter4++;
+        count4++;
     }
-    else if (sign == '5')
+    else if (digit == '5')
     {
-        counter5++;
+        count5++;
     }
-    else if (sign == '6')
+    else if (digit == '6')
     {
-        counter6++;
+        count6++;
     }
-    else if (sign == '7')
+    else if (digit == '7')
     {
-        counter7++;
+        count7++;
     }
-    else if (sign == '8')
+    else if (digit == '8')
     {
-        counter8++;
+        count8++;
     }
-    else if (sign == '9')
+    else if (digit == '9')
     {
-        counter9++;
+        count9++;
     }
 }
 
-Console.WriteLine("Wynik dla podanej liczby");
-Console.WriteLine(" 0 -> " + counter0);
-Console.WriteLine(" 1 -> " + counter1);
-Console.WriteLine(" 2 -> " + counter2);
-Console.WriteLine(" 3 -> " + counter3);
-Console.WriteLine(" 4 -> " + counter4);
-Console.WriteLine(" 5 -> " + counter5);
-Console.WriteLine(" 6 -> " + counter6);
-Console.WriteLine(" 7 -> " + counter7);
-Console.WriteLine(" 8 -> " + counter8);
-Console.WriteLine(" 9 -> " + counter9);
+Console.WriteLine("Wynik dla podanej liczby: " + number);
+Console.WriteLine("cyfra 0 -> " + count0);
+Console.WriteLine("cyfra 1 -> " + count1);
+Console.WriteLine("cyfra 2 -> " + count2);
+Console.WriteLine("cyfra 3 -> " + count3);
+Console.WriteLine("cyfra 4 -> " + count4);
+Console.WriteLine("cyfra 5 -> " + count5);
+Console.WriteLine("cyfra 6 -> " + count6);
+Console.WriteLine("cyfra 7 -> " + count7);
+Console.WriteLine("cyfra 8 -> " + count8);
+Console.WriteLine("cyfra 9 -> " + count9);
