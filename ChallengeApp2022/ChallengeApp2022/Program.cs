af4fb8a512327431b25f060012a64801921537cf
iMedia SysteM <imediasystem@hotmail.com>
2024-01-14 19:19:03 +0100

    Zadanie domowe Day 6

--- ChallengeApp2022/ChallengeApp2022/Program.cs
+++ ChallengeApp2022/ChallengeApp2022/Program.cs
@@ -1,72 +1,43 @@
-﻿// Zadanie domowe Day 5
+﻿// Zadanie domowe Day 6
 
-int number = 3633;
-string numberAsString = number.ToString();
-char[] digits = numberAsString.ToArray();
+using ChallengeApp2022;
 
-int count0 = 0;
-int count1 = 0;
-int count2 = 0;
-int count3 = 0;
-int count4 = 0;
-int count5 = 0;
-int count6 = 0;
-int count7 = 0;
-int count8 = 0;
-int count9 = 0;
+Worker worker1 = new Worker("Adam", "Nowak", 32);
+Worker worker2 = new Worker("Karol", "Theus", 34);
+Worker worker3 = new Worker("Jakub", "Kowalski", 27);
 
-foreach (char digit in digits)
+
+worker1.AddRate(5);
+worker1.AddRate(4);
+worker1.AddRate(7);
+
+worker2.AddRate(6);
+worker2.AddRate(7);
+worker2.AddRate(8);
+
+worker3.AddRate(9);
+worker3.AddRate(6);
+worker3.AddRate(7);
+
+
+List<Worker> workers = new List<Worker>()
 {
-    if (digit == '0')
+    worker1, worker2, worker3
+};
+
+int maxResult = -1;
+Worker workerWithMaxResult = null;
+
+foreach (var worker in workers)
+{
+    if (worker.Result > maxResult)
+
     {
-        count0++;
-    }
-    else if (digit == '1')
-    {
-        count1++;
-    }
-    else if (digit == '2')
-    {
-        count2++;
-    }
-    else if (digit == '3')
-    {
-        count3++;
-    }
-    else if (digit == '4')
-    {
-        count4++;
-    }
-    else if (digit == '5')
-    {
-        count5++;
-    }
-    else if (digit == '6')
-    {
-        count6++;
-    }
-    else if (digit == '7')
-    {
-        count7++;
-    }
-    else if (digit == '8')
-    {
-        count8++;
-    }
-    else if (digit == '9')
-    {
-        count9++;
+        workerWithMaxResult = worker;
+        maxResult = worker.Result;
     }
 }
 
-Console.WriteLine("Wynik dla podanej liczby: " + number);
-Console.WriteLine("cyfra 0 -> " + count0);
-Console.WriteLine("cyfra 1 -> " + count1);
-Console.WriteLine("cyfra 2 -> " + count2);
-Console.WriteLine("cyfra 3 -> " + count3);
-Console.WriteLine("cyfra 4 -> " + count4);
-Console.WriteLine("cyfra 5 -> " + count5);
-Console.WriteLine("cyfra 6 -> " + count6);
-Console.WriteLine("cyfra 7 -> " + count7);
-Console.WriteLine("cyfra 8 -> " + count8);
-Console.WriteLine("cyfra 9 -> " + count9);
+Console.WriteLine("Pracownikiem z najlepszym wynikiem zostaje" + " " + workerWithMaxResult.Name + " "
+    + workerWithMaxResult.Surname + " " + "lat" + " " + workerWithMaxResult.Age + ", "
+    + "który zdobył" + " " + workerWithMaxResult.Result + " " + "pkt");
